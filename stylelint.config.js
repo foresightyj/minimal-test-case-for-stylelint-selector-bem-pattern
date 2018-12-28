2ed94410604475ed05f66776f53161dc731b011c
Yuan Jian <imyuanjian@gmail.com>
2018-12-28 15:05:38 +0800

    minimal test case for stylelint-selector-bem-pattern

---
+++ stylelint.config.js
@@ -0,0 +1,21 @@
+﻿const allowedPrimitiveTags = ["h[1-6]", "ul", "li", "a", "p", "label", "input", "textarea", "select", "option", "button", "fieldset"].map(t=>new RegExp(`^${t}$`));
+const allowedThirdPartyClassPrefix = ["ui-", "sa-", "fa-"].map(p=> new RegExp(`^\\.${p}.+`));
+const optionalAttributeSelector ="(\\[.+)?"
+
+module.exports = {
+    "plugins": [
+        "stylelint-selector-bem-pattern"
+    ],
+    "rules": {
+        "plugin/selector-bem-pattern": {
+            "componentName": "^[A-Za-z]+",
+            "componentSelectors": {
+                "initial": `^\\.{componentName}(?:__.+|--.+|\\[.+)?$`,
+                "combined": `^(\\.{componentName}(?:__.+|--.+)?)${optionalAttributeSelector}$`
+            },
+            // implicitComponents: ['**/module*.scss'],
+            "utilitySelectors": "^\\.util-[a-z]+$",
+            "ignoreSelectors": allowedPrimitiveTags.concat(allowedThirdPartyClassPrefix)
+        }
+    }
+}
